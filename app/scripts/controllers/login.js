48fc1299d7c1a3d3cb598cad301f0b166503bb62
Sylvestre <sbouchot@enssat.fr>
2016-10-14 14:42:33 +0200

    Intercept request to put my token

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -9,42 +9,43 @@
  */
 angular.module('appApp')
 .controller('LoginCtrl', function ($rootScope, $scope, $window, myVilleAPI, localStorageService, hello) {
-  $scope.user = {};
+	$scope.user = {};
 
-  $scope.loginClick = function() {
-
-    if($scope.user.username || $scope.user.password){
-        myVilleAPI.User.login($scope.user).then(function(user){
-          if(!user.data.user) return $scope.message = 'Mauvaise combinaison';
-          $rootScope.token = user.data.token;
-          $rootScope.user = user.data.user;
-          localStorageService.set('token', user.data.token);
-          localStorageService.set('user', user.data.user);
-          $scope.message = '';
-          $scope.closeThisDialog();
-        }, function(error){
-          $scope.message = error.data.message;
-          console.log(error.data);
-        });
-    } else {
-      $scope.message = 'Error, fiels needed.';
-    }
-  };
-  $scope.loginFB = function(){
-    hello('facebook').login({scope: 'basic,email'}).then(function(auth){
-    	if(auth.network === 'facebook') {
-	      myVilleAPI.User.loginFacebook({accessToken: auth.authResponse.access_token}).then(function(user){
-	        $rootScope.token = user.data.token;
-	        $rootScope.user = user.data.user;
-	        localStorageService.set('token', user.data.token);
-	        localStorageService.set('user', user.data.user);
-	        $scope.closeThisDialog();
-	      });
-    	}
-    });
-  };
-  $scope.loginTwitter = function(){
-    hello('twitter').login();
-  };
+	$scope.loginClick = function() {
+		if($scope.user.username || $scope.user.password){
+				myVilleAPI.User.login($scope.user).then(function(user){
+					if(!user.data.user) return $scope.message = 'Mauvaise combinaison';
+					$rootScope.token = user.data.token;
+					$rootScope.user = user.data.user;
+					localStorageService.set('token', user.data.token);
+					localStorageService.set('expiryToken', new Date() + 86400000);
+					localStorageService.set('user', user.data.user);
+					$scope.message = '';
+					$scope.closeThisDialog();
+				}, function(error){
+					$scope.message = error.data.message;
+					console.log(error.data);
+				});
+		} else {
+			$scope.message = 'Error, fiels needed.';
+		}
+	};
+	$scope.loginFB = function(){
+		hello('facebook').login({scope: 'basic,email'}).then(function(auth){
+			if(auth.network === 'facebook') {
+				myVilleAPI.User.loginFacebook({accessToken: auth.authResponse.access_token}).then(function(user){
+					$rootScope.token = user.data.token;
+					$rootScope.user = user.data.user;
+					localStorageService.set('token', user.data.token);
+					localStorageService.set('expiryToken', new Date() + 86400000);
+					localStorageService.set('user', user.data.user);
+					$scope.closeThisDialog();
+				});
+			}
+		});
+	};
+	$scope.loginTwitter = function(){
+		hello('twitter').login();
+	};
 
 });
