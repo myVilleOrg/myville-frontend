f81e64de778000eb0022818b15475802e37dd8e3
Sylvestre <sbouchot@enssat.fr>
2016-12-16 11:35:06 +0100

    Forgot password

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -9,75 +9,87 @@
 
 angular.module('appApp').controller('LoginCtrl', function ($rootScope, $scope, $window, myVilleAPI, AuthentificationService, hello, ngDialog, $location) {
 
-  $scope.user = {};
-  $scope.signupUser = {};
-  $scope.log = true;
+	$scope.user = {};
+	$scope.signupUser = {};
+	$scope.forgotPwd = {};
+	$scope.log = true;
 
-  $scope.loginClick = function() {
+	$scope.loginClick = function() {
+		if($scope.user.username || $scope.user.password){
+				myVilleAPI.User.login($scope.user).then(function(user){
+					if(!user.data.user) return $scope.message = 'Mauvaise combinaison';
 
-    if($scope.user.username || $scope.user.password){
-        myVilleAPI.User.login($scope.user).then(function(user){
-          if(!user.data.user) return $scope.message = 'Mauvaise combinaison';
+					AuthentificationService.login(user.data.token, user.data.user);
+					$scope.message = '';
+					$scope.closeThisDialog();
+				}, function(error){
+					$scope.message = error.data.message;
+				});
+		} else {
+			$scope.message = 'Error, fiels needed.';
+		}
+	};
 
-  				AuthentificationService.login(user.data.token, user.data.user);
-          $scope.message = '';
-          $scope.closeThisDialog();
-        }, function(error){
-          $scope.message = error.data.message;
-        });
-    } else {
-      $scope.message = 'Error, fiels needed.';
-    }
-  };
+	$scope.switchMode = function (mode){
+		$scope.message = '';
 
-  $scope.switchMode = function (){
-    $scope.message = '';
-  	if($scope.log) $scope.log = false
-  	else $scope.log = true;
-  }
+		if(mode === 'login') $scope.log = 1;
+		if(mode === 'signup') $scope.log = 2;
+		if(mode === 'forgot') $scope.log = 3;
+	}
 
-  $scope.createClick = function(){
+	$scope.createClick = function(){
 
-    if(!$scope.signupUser.nickname || !$scope.signupUser.password || !$scope.signupUser.email || !$scope.signupUser.phonenumber){
-      return $scope.message = 'Un ou des champs sont manquant.';
-    }
+		if(!$scope.signupUser.nickname || !$scope.signupUser.password || !$scope.signupUser.email || !$scope.signupUser.phonenumber){
+			return $scope.message = 'Un ou des champs sont manquant.';
+		}
 		var data = {
-		  username: $scope.signupUser.nickname,
-		  password: $scope.signupUser.password,
-		  email: $scope.signupUser.email,
-		  phonenumber: $scope.signupUser.phonenumber
+			username: $scope.signupUser.nickname,
+			password: $scope.signupUser.password,
+			email: $scope.signupUser.email,
+			phonenumber: $scope.signupUser.phonenumber
 		};
 
 		myVilleAPI.User.create(data).then(function(user){
 			AuthentificationService.login(user.data.token, user.data.user);
-	  });
+		});
 
-    ngDialog.close();
-    $location.path('/');
-  };
+		ngDialog.close();
+		$location.path('/');
+	};
 
-  $scope.loginFB = function(){
-    hello('facebook').login({scope: 'basic,email'}).then(function(auth){
-    	if(auth.network === 'facebook') {
-	      myVilleAPI.User.loginFacebook({accessToken: auth.authResponse.access_token}).then(function(user){
-  				AuthentificationService.login(user.data.token, user.data.user);
-	        $scope.closeThisDialog();
-	      });
-    	}
-    });
-  };
-  $scope.loginGoogle = function(){
-    hello('google').login({scope: 'basic,email'}).then(function(auth){
-      if(auth.network === 'google'){
-        myVilleAPI.User.loginGoogle({accessToken: auth.authResponse.access_token}).then(function(user){
-  				AuthentificationService.login(user.data.token, user.data.user);
-          $scope.closeThisDialog();
-        });
-      }
-    });
-  };
-  $scope.loginTwitter = function(){
-    hello('twitter').login();
-  };
+	$scope.forgotClick = function(){
+		if(!$scope.forgotPwd.email) return $scope.message = 'Un ou des champs sont manquant.';
+		myVilleAPI.User.forgot({email: $scope.forgotPwd.email}).then(function(data){
+			$scope.switchMode('login');
+			$scope.message = 'Mail de récupération de mot de passe envoyé !';
+		}).catch(function(err){
+			$scope.message = err.data.message;
+		});
+	};
+
+	$scope.loginFB = function(){
+		hello('facebook').login({scope: 'basic,email'}).then(function(auth){
+			if(auth.network === 'facebook') {
+				myVilleAPI.User.loginFacebook({accessToken: auth.authResponse.access_token}).then(function(user){
+					AuthentificationService.login(user.data.token, user.data.user);
+					$scope.closeThisDialog();
+				});
+			}
+		});
+	};
+	$scope.loginGoogle = function(){
+		hello('google').login({scope: 'basic,email'}).then(function(auth){
+			if(auth.network === 'google'){
+				myVilleAPI.User.loginGoogle({accessToken: auth.authResponse.access_token}).then(function(user){
+					AuthentificationService.login(user.data.token, user.data.user);
+					$scope.closeThisDialog();
+				});
+			}
+		});
+	};
+	$scope.loginTwitter = function(){
+		hello('twitter').login();
+	};
 
 });
--- app/scripts/services/myvilleapi.js
+++ app/scripts/services/myvilleapi.js
@@ -8,58 +8,63 @@
  * Factory in the appApp.
  */
 angular.module('appApp')
-  .factory('myVilleAPI', ['$http', '$rootScope', function ($http, $rootScope) {
+	.factory('myVilleAPI', ['$http', '$rootScope', function ($http, $rootScope) {
 
-      var baseUrl = 'http://localhost:3000';
-      var dataFactory = {
-        User: {
-          login: function(data){
-            return $http.post(baseUrl + '/user/login', data);
-          },
-          loginFacebook: function(data) {
-            return $http.post(baseUrl + '/user/login/facebook', data);
-          },
-          loginGoogle: function(data) {
-            return $http.post(baseUrl + '/user/login/google', data);
-          },
-          create: function(data){
-            return $http.post(baseUrl + '/user/create', data);
-          },
-          update: function(data) {
-            return $http.put(baseUrl + '/user/update', data);
-          },
-          updateAvatar: function(data) {
-            return $http.post(baseUrl + '/user/update/avatar', data, {transformRequest: angular.identify, headers: {'Content-Type': undefined, enctype:'multipart/form-data'}});	
-          },
-          get: function(data){
-          	return $http.get(baseUrl + '/user/' + data);
-          }
+			var baseUrl = 'http://localhost:3000';
+			var dataFactory = {
+				User: {
+					login: function(data){
+						return $http.post(baseUrl + '/user/login', data);
+					},
+					loginFacebook: function(data) {
+						return $http.post(baseUrl + '/user/login/facebook', data);
+					},
+					loginGoogle: function(data) {
+						return $http.post(baseUrl + '/user/login/google', data);
+					},
+					create: function(data){
+						return $http.post(baseUrl + '/user/create', data);
+					},
+					update: function(data) {
+						return $http.put(baseUrl + '/user/update', data);
+					},
+					updateAvatar: function(data) {
+						return $http.post(baseUrl + '/user/update/avatar', data, {transformRequest: angular.identify, headers: {'Content-Type': undefined, enctype:'multipart/form-data'}});
+					},
+					get: function(data){
+						return $http.get(baseUrl + '/user/' + data);
+					},
+					forgot: function(data){
+						return $http.post(baseUrl + '/user/forgetPassword', data);
+					},
+					reset: function(data) {
+						return $http.post(baseUrl + '/user/reset', data);
+					}
+				},
 
-        },
-
-        UAS: {
-        	get: function(data){
+				UAS: {
+					get: function(data){
 						return $http.get(baseUrl + '/ua/get/geo', {params: data});
-        	},
-          create: function(data){
-            return $http.post(baseUrl + '/ua/create', data);
-          },
-        	getPopular: function(data){
+					},
+					create: function(data){
+						return $http.post(baseUrl + '/ua/create', data);
+					},
+					getPopular: function(data){
 						return $http.get(baseUrl + '/ua/get/geo', {params: data});
-        	},
-        	getMine: function(){
-        		return $http.get(baseUrl + '/ua/get/mine');
-        	},
-        	getOne: function(id){
-        		return $http.get(baseUrl + '/ua/' + id);
-        	},
-          favor: function(data){
-            return $http.post(baseUrl + '/ua/favor', data); 
-          },
-        	update: function(id, data){
+					},
+					getMine: function(){
+						return $http.get(baseUrl + '/ua/get/mine');
+					},
+					getOne: function(id){
+						return $http.get(baseUrl + '/ua/' + id);
+					},
+					favor: function(data){
+						return $http.post(baseUrl + '/ua/favor', data);
+					},
+					update: function(id, data){
 							return $http.put(baseUrl + '/ua/' + id, data);
-        	}
-        }
-      };
-      return dataFactory;
-    }]);
+					}
+				}
+			};
+			return dataFactory;
+		}]);
